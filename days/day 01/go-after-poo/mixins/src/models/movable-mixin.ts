5a6909efd0a3916709e27372b94b6fea29881d5a
evan boissonnot <evan@boissonnot.fr>
2024-11-13 22:25:36 +0100

    feat: mixin with constraints

--- days/day 01/go-after-poo/mixins/src/models/movable-mixin.ts
+++ days/day 01/go-after-poo/mixins/src/models/movable-mixin.ts
@@ -15,3 +15,24 @@ export function Movable<TBase extends Constructor>(Base: TBase) {
 }
 
 export const MovableEnemy = Movable(Enemy);
+
+
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type, @typescript-eslint/no-explicit-any
+type BetterConstructor<T = {}> = new (...args: any[]) => T;
+
+type EnemyConstructor = BetterConstructor<Enemy>;
+
+function EnemyMovable<TBase extends EnemyConstructor>(Base: TBase) {
+    return class extends Base {
+        position = { x: 0, y: 0 };
+
+        move(x: number, y: number) {
+            this.position.x += x;
+            this.position.y += y;
+
+            this.name = "Movable " + this.name;
+        }
+    }
+}
+
+export const MovableEnemy2 = EnemyMovable(Enemy);
