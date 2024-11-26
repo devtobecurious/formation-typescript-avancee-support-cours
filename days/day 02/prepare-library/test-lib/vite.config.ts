5bc281a2de20bfb19975c41294b8613ac3785ba5
Evan BOISSONNOT <evan@boissonnot.fr>
2024-11-26 09:04:47 +0100

    feat: prepare library

---
+++ days/day 02/prepare-library/test-lib/vite.config.ts
@@ -0,0 +1,28 @@
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  // plugins: [dts()],
+  build: {
+    lib: {
+      // Could also be a dictionary or array of multiple entry points
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'Player',
+      // the proper extensions will be added
+      fileName: 'player-lib',
+      formats: ['cjs', 'es', 'umd']
+    },
+    rollupOptions: {
+      // make sure to externalize deps that shouldn't be bundled
+      // into your library
+      // external: ['vue'],
+    //   output: {
+    //     // Provide global variables to use in the UMD build
+    //     // for externalized deps
+    //     globals: {
+    //       vue: 'Vue',
+    //     },
+    //   },
+    },
+  },
+})
